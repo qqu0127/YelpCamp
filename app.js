68928ce7f3859b061de22c437e951550fd159ce1
Quincy Qu <qqu0127@gmail.com>
2018-08-14 17:51:18 +0000

    add more style

--- app.js
+++ app.js
@@ -5,6 +5,18 @@ var bodyParser = require("body-parser");
 var cmp = [
     {name: "Salmon Creek", image: "https://recreation-acm.activefederal.com/assetfactory.aspx?did=7460"},
     {name: "Granite Hill", image: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQQFfqSyzNCa9DdWrI4yEoCEceqxBJ7U-pa5wKH48Bv4vETx48W"},
+    {name: "Mountain Goat's Rest", image: "http://www.westernlehigh.org/wp-content/uploads/2016/02/campingoutside.png"},
+    {name: "Salmon Creek", image: "https://recreation-acm.activefederal.com/assetfactory.aspx?did=7460"},
+    {name: "Granite Hill", image: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQQFfqSyzNCa9DdWrI4yEoCEceqxBJ7U-pa5wKH48Bv4vETx48W"},
+    {name: "Mountain Goat's Rest", image: "http://www.westernlehigh.org/wp-content/uploads/2016/02/campingoutside.png"},
+    {name: "Salmon Creek", image: "https://recreation-acm.activefederal.com/assetfactory.aspx?did=7460"},
+    {name: "Granite Hill", image: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQQFfqSyzNCa9DdWrI4yEoCEceqxBJ7U-pa5wKH48Bv4vETx48W"},
+    {name: "Mountain Goat's Rest", image: "http://www.westernlehigh.org/wp-content/uploads/2016/02/campingoutside.png"},
+    {name: "Salmon Creek", image: "https://recreation-acm.activefederal.com/assetfactory.aspx?did=7460"},
+    {name: "Granite Hill", image: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQQFfqSyzNCa9DdWrI4yEoCEceqxBJ7U-pa5wKH48Bv4vETx48W"},
+    {name: "Mountain Goat's Rest", image: "http://www.westernlehigh.org/wp-content/uploads/2016/02/campingoutside.png"},
+    {name: "Salmon Creek", image: "https://recreation-acm.activefederal.com/assetfactory.aspx?did=7460"},
+    {name: "Granite Hill", image: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQQFfqSyzNCa9DdWrI4yEoCEceqxBJ7U-pa5wKH48Bv4vETx48W"},
     {name: "Mountain Goat's Rest", image: "http://www.westernlehigh.org/wp-content/uploads/2016/02/campingoutside.png"}
 ];
 
